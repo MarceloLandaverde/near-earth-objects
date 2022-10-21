1f0879f0cde7c055f15ce03ea64cf06e1e075a2e
MarceloLandaverde <39967857+MarceloLandaverde@users.noreply.github.com>
2022-10-21 09:35:23 +0200

    Add files via upload

---
+++ write.py
@@ -0,0 +1,76 @@
+"""Write a stream of close approaches to CSV or to JSON.
+
+This module exports two functions: `write_to_csv` and `write_to_json`, each of
+which accept an `results` stream of close approaches and a path to which to
+write the data.
+
+These functions are invoked by the main module with the output of the `limit`
+function and the filename supplied by the user at the command line. The file's
+extension determines which of these functions is used.
+
+You'll edit this file in Part 4.
+"""
+import csv
+import json
+from helpers import datetime_to_str
+
+def write_to_csv(results, filename):
+    """Write an iterable of `CloseApproach` objects to a CSV file.
+
+    The precise output specification is in `README.md`. Roughly, each output row
+    corresponds to the information in a single close approach from the `results`
+    stream and its associated near-Earth object.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
+    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    rows = [list(fieldnames)]
+    
+    for a in results:
+        r = [datetime_to_str(a.time),a.distance, a.velocity, a.neo.designation, a.neo.name, a.neo.diameter, a.neo.hazardous]
+        
+        if r[4] == None:
+            r[4] = ""
+        r[6] = str(r[6]).capitalize()
+    
+        rows.append(r)
+        
+    with open(filename,"w") as outfile:
+        writer = csv.writer(outfile,lineterminator="\n")
+        for r in rows:
+            writer.writerow(r)
+    
+
+def write_to_json(results, filename):
+    """Write an iterable of `CloseApproach` objects to a JSON file.
+
+    The precise output specification is in `README.md`. Roughly, the output is a
+    list containing dictionaries, each mapping `CloseApproach` attributes to
+    their values and the 'neo' key mapping to a dictionary of the associated
+    NEO's attributes.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    top_level = []
+    for a in results:
+        d = {}
+        d["datetime_utc"] = datetime_to_str(a.time)
+        d["distance_au"] = a.distance
+        d["velocity_km_s"] = a.velocity
+        d["neo"] = {"designation":a.neo.designation ,
+                    "name": a.neo.name,
+                    "diameter_km":a.neo.diameter,
+                    "potentially_hazardous": a.neo.hazardous}
+
+        top_level.append(d)
+
+    for e in top_level:
+        if e["neo"]["name"] == None:
+            e["neo"]["name"] = ""
+    
+    with open(filename,"w") as outfile:
+        json.dump(top_level,outfile,indent=2)
